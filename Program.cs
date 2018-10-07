6c3a57a8784ccb55fa95684e0f53604eff99bc45
Duch003 <tomaszmankin003@gmail.com>
2018-10-07 02:54:35 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VocabularyDatabase;
+
+namespace VocabularyTrainer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var vocab = new QuestionsSet();
+            vocab.GetAllRecords();
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+    }
+}
